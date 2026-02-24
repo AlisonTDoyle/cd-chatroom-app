252e32804f7b95af1ac7744dd4c80fa279b81479
S00233102 <S00233102@atu.ie>
2026-02-24 16:40:07 +0000

    Refactor Auth service structure and add ApiService with tests

--- src/app/routes/chat/chat.ts
+++ src/app/routes/chat/chat.ts
@@ -3,7 +3,7 @@ import { ChatList } from '../../components/chat/chat-list/chat-list';
 import { Chatroom } from '../../components/chat/chatroom/chatroom';
 import { ChatInfo } from '../../components/chat/chat-info/chat-info';
 import { MessageBox } from "../../components/chat/message-box/message-box";
-import { Auth } from '../../services/auth';
+import { Auth } from '../../services/auth/auth';
 import { CommonModule } from '@angular/common';
 
 @Component({
--- src/app/routes/login/login.ts
+++ src/app/routes/login/login.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Auth } from '../../services/auth';
+import { Auth } from '../../services/auth/auth';
 import { Router } from '@angular/router';
 
 @Component({
